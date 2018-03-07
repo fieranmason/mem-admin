9f36cc274a17dba024ab633c672e51d2cb82925e
Fieran Mason <fieranmason@gmail.com>
2018-03-06 17:01:40 -0800

    Configured the LoginSpec to use a source code based data fixture

--- functional-tests/src/test/groovy/login/LoginSpec.groovy
+++ functional-tests/src/test/groovy/login/LoginSpec.groovy
@@ -1,3 +1,11 @@
+import com.mongodb.MongoClient
+import com.mongodb.DB
+import com.mongodb.DBCollection
+import com.mongodb.DBCursor
+import com.mongodb.DBObject
+import com.mongodb.BasicDBObject
+import com.mongodb.WriteConcern
+
 import geb.spock.GebReportingSpec
 import spock.lang.Narrative
 import spock.lang.Title
@@ -12,13 +20,50 @@ import pages.app.LoginPage
 @Title('Feature: Authentication')
 
 class LoginSpec extends GebReportingSpec {
+  static _host = "localhost"
+  static _port = 27017
+  static _database = "mem-dev-func"
+  static MongoClient _mongoClient
+  static _username = "username"
+  static _password = "password"
 
   def setupSpec() {
-    //no spec setup
+    _mongoClient = _mongoClient ?: new MongoClient(_host, _port)
+    DB db = _mongoClient.getDB(_database)
+    DBCollection users = db.getCollection('users')
+
+    BasicDBObject user = new BasicDBObject()
+    user.put("username", _username)
+    user.put("password", _password)
+    users.insert(user, WriteConcern.SAFE)
+  }
+
+  def inspect(collectionName, collection) {
+    println "collection: " + collectionName
+    println "#" + collectionName + ": " + collection.count()
+
+    DBCursor cursor = collection.find();
+
+    println "---start " + collectionName + " collection ---"
+    while( cursor.hasNext() ) {
+       DBObject obj = cursor.next();
+       println obj
+    }
+    println "---end " + collectionName + " colection---"
+
   }
 
   def cleanupSpec() {
-    //no spec cleanup
+    _mongoClient = _mongoClient ?: new MongoClient(_host, _port)
+    DB db = _mongoClient.getDB(_database)
+    DBCollection users = db.getCollection('users')
+
+    DBCursor cursor = users.find();
+
+    while( cursor.hasNext() ) {
+      DBObject obj = cursor.next();
+      users.remove(obj, WriteConcern.SAFE)
+    }
   }
 
   def setup() {
@@ -36,7 +81,6 @@ class LoginSpec extends GebReportingSpec {
     given:
       "A user goes to the home page"
       to HomePage
-      waitFor { at(HomePage) }
     when: "and the user attempts to login with valid credentials"
       header_nav_bar_toggle.click()
       header_login_link.click()
@@ -44,22 +88,30 @@ class LoginSpec extends GebReportingSpec {
       assert false
   }
 
-  //Test header login link
-  @Unroll
-  def "Scenario 1B: Access login page through footer link"() {
+  //Test footer login link
+  def "Scenario 1B: Access login page through footer local link"() {
     given:
       "A user goes to the home page"
       to HomePage
-      waitFor { at(HomePage) }
     when: "and accesses the login page through the header"
       footer_login_link.click();
-    then: "the user is authenticated and provided access to the admin features."
-      assert at(LoginPage)
+    then: "the user is redirected to the authentication page."
+      at LoginPage
   }
 
   //Test authentication
   def "Scenario 2: Authenticate"() {
-    assert false
+    given: "A user accesses the authentication page"
+      to LoginPage
+    when: "and the users enters and submits valid credentials"
+      username.value( _username )
+      password.value( _password )
+
+      println authenticateButton
+      authenticateButton.click()
+    then: "the user is authenticated and redirected to the homepage"
+      at HomePage
+      footer_logout_link
   }
 
 }
